d2bb80319298e64212ed5eb55f3ab04365a97d12
Matheus Baldanca <baldancam@gmail.com>
2023-12-12 16:50:22 -0300

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <GlobalStyles />
+    </BrowserRouter>
   );
 }
 
